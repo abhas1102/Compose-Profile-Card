2b2781e7b8164cb4d4035a0a257c8eb96790155f
Abhas Kumar <abhas1102@gmail.com>
2021-11-09 02:06:05 +0530

    Created dynamic lists of users in compose

--- app/src/main/java/com/example/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/example/profilecardlayout/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,12 +35,17 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun MainScreen() {
+    fun MainScreen(userProfiles:List<UserProfile> = userProfileList) {
 
         Scaffold(topBar = {AppBar()}) {
             androidx.compose.material.Surface(modifier = Modifier.fillMaxSize(),
             ) {
-                ProfileCard()
+                Column {
+                    for(i in userProfiles){
+                        ProfileCard(userProfile = i)
+                    }
+                }
+
 
             }
             
@@ -55,25 +61,28 @@ class MainActivity : ComponentActivity() {
 
 
     @Composable
-    fun ProfileCard(){
+    fun ProfileCard(userProfile: UserProfile){
         Card(modifier = Modifier
-            .padding(16.dp)
+            .padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp)
             .fillMaxWidth()
             .wrapContentHeight(align = Alignment.Top),elevation = 8.dp,backgroundColor = Color.White) {
 
             Row(modifier = Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start) {
-                ProfilePicture()
-                ProfileContent()
+                ProfilePicture(userProfile.drawableId,userProfile.status)
+                ProfileContent(userProfile.name, userProfile.status)
             }
 
         }
     }
     @Composable
-    fun ProfilePicture(){
-        Card(shape = CircleShape, border = BorderStroke(width = 2.dp, color = MaterialTheme.colors.lightGreen),
+    fun ProfilePicture(drawableId:Int, onlineStatus:Boolean){
+        Card(shape = CircleShape, border = BorderStroke(width = 2.dp, color = if (onlineStatus)
+                                                                                MaterialTheme.colors.lightGreen
+                                                                                else Color.Red
+        ),
         modifier = Modifier.padding(16.dp),elevation = 4.dp) {
-            Image(painter = painterResource(id = R.drawable.neweasyday),
+            Image(painter = painterResource(id = drawableId),
                 contentDescription ="Content description" ,
                 modifier = Modifier.size(72.dp),contentScale = ContentScale.Crop)
 
@@ -82,14 +91,24 @@ class MainActivity : ComponentActivity() {
 
     }
     @Composable
-    fun ProfileContent(){
+    fun ProfileContent(userName:String, onlineStatus: Boolean){
         Column(modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()) {
-            Text(text = "John Doe",style = MaterialTheme.typography.h5)
+            CompositionLocalProvider(LocalContentAlpha provides (if (onlineStatus)
+                                                                            1f else ContentAlpha.medium)){
+                Text(text = userName,style = MaterialTheme.typography.h5)
+
+            }
 
 
-            Text(text = "Active Now", style = MaterialTheme.typography.body2)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium){
+                Text(text = if (onlineStatus)
+                    "Active Now"
+                else "Offline"
+                    , style = MaterialTheme.typography.body2)
+            }
+
 
         }
 
---
+++ app/src/main/java/com/example/profilecardlayout/UserProfile.kt
@@ -0,0 +1,9 @@
+package com.example.profilecardlayout
+
+data class UserProfile(val name:String, val status:Boolean, val drawableId:Int)
+
+val userProfileList = arrayListOf<UserProfile>(
+    UserProfile("John Doe", true,R.drawable.neweasyday),
+    UserProfile("Anna Johns", false,R.drawable.neweasyday)
+
+)
